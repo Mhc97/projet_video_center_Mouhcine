a006958e18ca455c624858447cafe04ba38c7f97
Mouhcine <mouhcine@gmail.com>
2026-07-31 12:27:14 +0200

    Fin S1 : CRUD complet (show, edit, delete) + templates + sécurité

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -21,7 +21,7 @@ class VideoController extends AbstractController
         ]);
     }
 
-    // AJOUTE CETTE MÉTHODE :
+    //  MÉTHODE AJOUTER !!:
     #[Route('/video/create', name: 'app_video_create')]
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -43,10 +43,36 @@ class VideoController extends AbstractController
     }
 
    #[Route('/video/{id}', name: 'app_video_show')]
-public function show(Video $video): Response
+    public function show(Video $video): Response
 {
     return $this->render('video/show.html.twig', [
         'video' => $video,
     ]);
 }
+
+#[Route('/video/{id}/edit', name: 'app_video_edit')]
+public function edit(Request $request, Video $video, EntityManagerInterface $em): Response
+{
+    $form = $this->createForm(VideoType::class, $video);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()){
+        $em->flush();
+        $this->addFlash('sucess', 'vidéo modifié !');
+        return $this->redirectToRoute('app_home');
+    }
+    return $this->render('video/edit.html.twig', ['form' => $form->createView(), 'video' => $video]);
 }
+
+#[Route('/video/{id}/delete', name: 'app_video_delete')]
+public function delete(Request $request, Video $video, EntityManagerInterface $em): Response
+{
+    if ($this->isCsrfTokenValid('delete' . $video->getId(), $request->request->get('_token'))) {
+        $em->remove($video);
+        $em->flush();
+        $this->addFlash('sucess', 'Vidéo supprimée !');
+
+    }
+    return $this->redirectToRoute('app_home');
+}
+}
+
